f82f2e0cac9afcd2ae095fcd5a219272147deefc
Partha Pratim Pradhan <partha.pradhan@mheducation.com>
2020-05-27 15:40:47 +0530

    Spring AOP annotation using before and after

--- src/main/java/com/partha/lrn/demo/config/ApplicationConfig.java
+++ src/main/java/com/partha/lrn/demo/config/ApplicationConfig.java
@@ -3,11 +3,13 @@ package com.partha.lrn.demo.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.partha.lrn.demo")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 	
 	
--- src/main/java/com/partha/lrn/demo/service/GreetingService.java
+++ src/main/java/com/partha/lrn/demo/service/GreetingService.java
@@ -3,13 +3,15 @@ package com.partha.lrn.demo.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.partha.lrn.demo.aspect.Loggable;
+
 @Service
 public class GreetingService {
 
 	 @Value("${app.greeting}")
 	    private String greeting;
 
-
+	@Loggable
     public String getGreeting(String name){
         return greeting + " " + name;
     }
